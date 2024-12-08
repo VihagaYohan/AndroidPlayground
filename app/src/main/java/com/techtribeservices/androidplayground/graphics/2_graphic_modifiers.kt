57040e6a7c2107f6b7ed95b99682aa5e50dcf7b8
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-09 04:40:48 +0530

    Added - clip and shape

--- app/src/main/java/com/techtribeservices/androidplayground/graphics/2_graphic_modifiers.kt
+++ app/src/main/java/com/techtribeservices/androidplayground/graphics/2_graphic_modifiers.kt
@@ -6,27 +6,33 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.techtribeservices.androidplayground.ui.theme.AndroidPlaygroundTheme
+import java.time.format.TextStyle
 
 @Composable
 fun GraphicModifiers() {
-    drawBehind()
+    clipAndShape()
 }
 
 @Composable
@@ -77,6 +83,32 @@ private fun drawBehind() {
                     )
                 }
                 .padding(10.dp)
+        )
+    }
+}
+
+@Composable
+private fun clipAndShape() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .graphicsLayer {
+                    clip = true
+                    shape = CircleShape
+                }
+                .background(Color.Red)
+        ) {
+            Text("Hello compoose",
+                style = androidx.compose.ui.text.TextStyle(color = Color.Black, fontSize = 46.sp),
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .background(Color.Blue)
         )
     }
 }
